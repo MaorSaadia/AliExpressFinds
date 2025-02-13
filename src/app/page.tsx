2b62751b0113a77df56c4412480e25883236c5df
Maor <maorsa9@gmail.com>
2025-02-13 11:15:06 +0200

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const Home: React.FC = async () => {
           <div className="relative text-center max-w-3xl mx-auto space-y-6">
             <div className="space-y-4 animate-fade-in">
               <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-gray-300 tracking-tight">
-                גלו מציאות מובילים
+                גלו מציאות מובילות
                 <span className="text-orange-500 dark:text-orange-300">
                   {" "}
                   במחירים ישירים
